05df354ded31b4bc25ce2eebb1c68d3a3dab6372
Tim Hoffman <timothy.hoffman@veridise.com>
2023-08-10 09:12:09 -0500

    add debug info to the unique id assertions

--- circuit_passes/src/passes/checks.rs
+++ circuit_passes/src/passes/checks.rs
@@ -1,138 +1,145 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use compiler::circuit_design::function::FunctionCode;
 use compiler::circuit_design::template::TemplateCode;
 use compiler::compiler_interface::Circuit;
-use compiler::intermediate_representation::{BucketId, Instruction, InstructionList, InstructionPointer};
-use compiler::intermediate_representation::ir_interface::{AddressType, LocationRule, LogBucketArg, ReturnType};
+use compiler::intermediate_representation::{
+    BucketId, Instruction, InstructionList, InstructionPointer,
+};
+use compiler::intermediate_representation::ir_interface::{
+    AddressType, LocationRule, LogBucketArg, ReturnType,
+};
 
-type Ids<'a> = &'a mut HashSet<BucketId>;
+type Ids<'a, 'b> = &'a mut HashMap<BucketId, &'b InstructionPointer>;
 
-fn assert_unique_ids_in_location_rule(location: &LocationRule, ids: Ids) {
+fn assert_unique_ids_in_location_rule<'a, 'b: 'a>(location: &'b LocationRule, ids: Ids<'a, 'b>) {
     match location {
         LocationRule::Indexed { location, .. } => assert_unique_ids_in_instruction(location, ids),
-        LocationRule::Mapped { indexes, .. } => assert_unique_ids_in_instruction_list(indexes, ids)
+        LocationRule::Mapped { indexes, .. } => assert_unique_ids_in_instruction_list(indexes, ids),
     }
 }
 
-fn assert_unique_ids_in_address_type(address: &AddressType, ids: Ids) {
+fn assert_unique_ids_in_address_type<'a, 'b: 'a>(address: &'b AddressType, ids: Ids<'a, 'b>) {
     match address {
         AddressType::Variable => {}
         AddressType::Signal => {}
-        AddressType::SubcmpSignal { cmp_address, .. } => assert_unique_ids_in_instruction(cmp_address, ids)
+        AddressType::SubcmpSignal { cmp_address, .. } => {
+            assert_unique_ids_in_instruction(cmp_address, ids)
+        }
     }
 }
 
-fn assert_unique_ids_in_return_type(return_type: &ReturnType, ids: Ids) {
+fn assert_unique_ids_in_return_type<'a, 'b: 'a>(return_type: &'b ReturnType, ids: Ids<'a, 'b>) {
     if let ReturnType::Final(data) = return_type {
         assert_unique_ids_in_location_rule(&data.dest, ids);
         assert_unique_ids_in_address_type(&data.dest_address_type, ids);
     }
 }
 
-fn assert_unique_ids_in_log_args(log_args: &LogBucketArg, ids: Ids) {
+fn assert_unique_ids_in_log_args<'a, 'b: 'a>(log_args: &'b LogBucketArg, ids: Ids<'a, 'b>) {
     if let LogBucketArg::LogExp(e) = log_args {
         assert_unique_ids_in_instruction(e, ids);
     }
 }
 
-fn assert_unique_ids_in_instruction(inst: &InstructionPointer, ids: Ids) {
+macro_rules! check_and_handle_ids {
+    ($inst: expr, $bucket: expr, $ids: expr) => {{
+        assert!(
+            !$ids.contains_key(&$bucket.id),
+            "Same ID for [{0:p}]{0:?} and [{1:p}]{1:?}",
+            *$inst,
+            **$ids.get(&$bucket.id).unwrap()
+        );
+        $ids.insert($bucket.id, $inst);
+    }};
+}
+
+fn assert_unique_ids_in_instruction<'a, 'b: 'a>(inst: &'b InstructionPointer, ids: Ids<'a, 'b>) {
     match inst.as_ref() {
         Instruction::Value(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
         }
         Instruction::Load(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_location_rule(&b.src, ids);
             assert_unique_ids_in_address_type(&b.address_type, ids);
         }
         Instruction::Store(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_address_type(&b.dest_address_type, ids);
             assert_unique_ids_in_location_rule(&b.dest, ids);
             assert_unique_ids_in_instruction(&b.src, ids);
         }
         Instruction::Compute(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction_list(&b.stack, ids);
         }
         Instruction::Call(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_return_type(&b.return_info, ids)
         }
         Instruction::Branch(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction(&b.cond, ids);
             assert_unique_ids_in_instruction_list(&b.if_branch, ids);
             assert_unique_ids_in_instruction_list(&b.else_branch, ids);
         }
         Instruction::Return(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction(&b.value, ids);
         }
         Instruction::Assert(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction(&b.evaluate, ids);
         }
         Instruction::Log(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             for arg in &b.argsprint {
                 assert_unique_ids_in_log_args(arg, ids);
             }
         }
         Instruction::Loop(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction(&b.continue_condition, ids);
             assert_unique_ids_in_instruction_list(&b.body, ids);
         }
         Instruction::CreateCmp(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction(&b.sub_cmp_id, ids);
         }
         Instruction::Constraint(b) => {
             assert_unique_ids_in_instruction(b.unwrap(), ids);
         }
         Instruction::Block(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
             assert_unique_ids_in_instruction_list(&b.body, ids);
         }
         Instruction::Nop(b) => {
-            assert!(!ids.contains(&b.id));
-            ids.insert(b.id);
+            check_and_handle_ids!(inst, b, ids);
         }
     }
 }
 
-fn assert_unique_ids_in_instruction_list(instructions: &InstructionList, ids: Ids) {
+fn assert_unique_ids_in_instruction_list<'a, 'b: 'a>(
+    instructions: &'b InstructionList,
+    ids: Ids<'a, 'b>,
+) {
     for inst in instructions {
         assert_unique_ids_in_instruction(inst, ids);
     }
 }
 
-fn assert_unique_ids_in_template(template: &TemplateCode, ids: Ids) {
+fn assert_unique_ids_in_template<'a, 'b: 'a>(template: &'b TemplateCode, ids: Ids<'a, 'b>) {
     assert_unique_ids_in_instruction_list(&template.body, ids);
 }
 
-fn assert_unique_ids_in_function(function: &FunctionCode, ids: Ids) {
+fn assert_unique_ids_in_function<'a, 'b: 'a>(function: &'b FunctionCode, ids: Ids<'a, 'b>) {
     assert_unique_ids_in_instruction_list(&function.body, ids);
-
 }
 
 /// Ensures that the ids of the buckets in a circuit are all unique
 /// Panics if not as duplicate ids are usually indicative of a bug in a pass
 pub fn assert_unique_ids_in_circuit(circuit: &Circuit) {
-    let mut visited: HashSet<BucketId> = Default::default();
+    let mut visited: HashMap<BucketId, &InstructionPointer> = HashMap::new();
     for template in &circuit.templates {
         assert_unique_ids_in_template(template, &mut visited);
     }
