17cbcb8341e290c3491cc4a7faaebea50eaff1b0
Henry Blanchette <henry@monkeyface.com>
2023-08-19 13:21:22 +0700

    checkpoint in reoganizing impl

--- code_producers/src/coda_elements/mod_new.rs
+++ code_producers/src/coda_elements/mod_new.rs
@@ -1,5 +1,7 @@
 // CodaCircuitData
 
+use super::ocaml::*;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodaCircuitData {
     pub field_tracking: Vec<String>,
@@ -13,19 +15,37 @@ impl Default for CodaCircuitData {
 
 // CodaProgram
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodaProgram {
     pub templates: Vec<CodaTemplate>,
 }
 
+impl CodaProgram {
+    pub fn coda_compile(&self) -> OcamlMod {
+        let stmts = self.templates.iter().map(|template| template.coda_compile()).collect();
+        OcamlMod { stmts }
+    }
+}
+
 // CodaTemplate
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodaTemplate {
     pub interface: CodaTemplateInterface,
     pub body: CodaStmt,
 }
 
+impl CodaTemplate {
+    pub fn coda_compile(&self) -> OcamlStmt {
+        let name = self.interface.name.coda_compile();
+        let body = self.body.coda_compile();
+        OcamlStmt::Define(name, body)
+    }
+}
+
 // CodaTemplateInterface
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodaTemplateInterface {
     pub id: usize,
     pub name: CodaTemplateName,
@@ -36,14 +56,19 @@ pub struct CodaTemplateInterface {
 
 // CodaTemplateSignal
 
+#[derive(Debug, PartialEq, Eq, Clone)]
+
 pub struct CodaTemplateSignal {}
 
 // CodaTemplateVariable
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 
 pub struct CodaTemplateVariable {}
 
 // CodaSubcomponent
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodaSubcomponent {
     pub interface: CodaTemplateInterface,
     pub name: CodaComponentName,
@@ -52,30 +77,191 @@ pub struct CodaSubcomponent {
 
 // CodaStmt
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum CodaStmt {
-    Let,
-    CreateSubcomponent,
-    Branch,
-    Assert,
+    Let(CodaNamed, CodaExpr, Box<CodaStmt>),
+    CreateSubcomponent(CodaSubcomponent, Box<CodaStmt>),
+    Branch(CodaExpr, Box<CodaStmt>, Box<CodaStmt>),
+    Assert(CodaExpr, CodaExpr, Box<CodaStmt>),
     Output,
+}
+
+impl CodaStmt {
+    fn coda_compile(&self) -> OcamlExpr {
+        match &self {
+            CodaStmt::Let(_, _, _) => todo!(),
+            CodaStmt::CreateSubcomponent(_, _) => todo!(),
+            CodaStmt::Branch(_, _, _) => todo!(),
+            CodaStmt::Assert(_, _, _) => todo!(),
+            CodaStmt::Output => todo!(),
+        }
+    }
 }
 
 // CodaExpr
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum CodaExpr {
-    Op,
-    Var,
-    Val,
-    Tuple,
+    Named(CodaNamed),
+    Value(CodaValue),
+    Op(CodaOp, Vec<Box<CodaExpr>>),
+    Tuple(Vec<Box<CodaExpr>>),
     Star,
+}
+
+impl CodaExpr {
+    pub fn coda_compile(&self) -> OcamlExpr {
+        match &self {
+            CodaExpr::Named(n) => n.coda_compile(),
+            CodaExpr::Value(v) => v.coda_compile(),
+            CodaExpr::Op(op, es) => {
+                OcamlExpr::op(op.coda_compile(), es.iter().map(|e| e.coda_compile()).collect())
+            }
+            CodaExpr::Tuple(es) => {
+                OcamlExpr::coda_tuple(es.iter().map(|e| e.coda_compile()).collect())
+            }
+            CodaExpr::Star => OcamlExpr::coda_star(),
+        }
+    }
+}
+
+// CodaName
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum CodaNamed {
+    Signal(CodaSignal),
+    SubcomponentSignal(CodaSubcomponentSignal),
+    Variable(CodaVariable),
+}
+
+impl CodaNamed {
+    fn coda_compile(&self) -> OcamlExpr {
+        match &self {
+            CodaNamed::Signal(s) => s.coda_compile(),
+            CodaNamed::SubcomponentSignal(s) => s.coda_compile(),
+            CodaNamed::Variable(x) => x.coda_compile(),
+        }
+    }
+}
+
+// CodaSignal
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct CodaSignal {
+    pub string: String,
+}
+
+impl CodaSignal {
+    fn coda_compile(&self) -> OcamlExpr {
+        OcamlExpr::var(OcamlName::new(&self.string))
+    }
+}
+
+// CodaSubcomponentSignal
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct CodaSubcomponentSignal {
+    pub subcomponent: CodaSubcomponent,
+    pub string: String,
+}
+
+impl CodaSubcomponentSignal {
+    fn coda_compile(&self) -> OcamlExpr {
+        let mut str = String::new();
+        str.push_str(&self.subcomponent.name.string);
+        str.push_str("__");
+        str.push_str(&self.string);
+        OcamlExpr::coda_var(OcamlExpr::string(&str))
+    }
+}
+
+// CodaVariable
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct CodaVariable {
+    pub string: String,
+}
+impl CodaVariable {
+    fn coda_compile(&self) -> OcamlExpr {
+        OcamlExpr::coda_var(OcamlExpr::string(&self.string))
+    }
+}
+
+// CodaValue
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct CodaValue {
+    pub string: String,
+}
+impl CodaValue {
+    fn coda_compile(&self) -> OcamlExpr {
+        OcamlExpr::coda_var(OcamlExpr::number(&self.string))
+    }
+}
+
+// CodaOp
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum CodaOp {
+    Add,
+    Sub,
+    Mul,
+    Pow,
+    Div,
+    Mod,
+    Eq,
+}
+
+impl CodaOp {
+    pub fn coda_compile(&self) -> OcamlOp {
+        let qual_field = Some(OcamlName::new("F"));
+        match &self {
+            CodaOp::Add => OcamlOp::new(qual_field, "+"),
+            CodaOp::Sub => OcamlOp::new(qual_field, "-"),
+            CodaOp::Mul => OcamlOp::new(qual_field, "*"),
+            CodaOp::Pow => OcamlOp::new(qual_field, "^"),
+            CodaOp::Div => OcamlOp::new(qual_field, "/"),
+            CodaOp::Mod => OcamlOp::new(qual_field, "%"),
+            CodaOp::Eq => OcamlOp::new(None, "=="),
+        }
+    }
 }
 
 // CodaTemplateName
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct CodaTemplateName {
     pub string: String,
+}
+impl CodaTemplateName {
+    fn coda_compile(&self) -> OcamlName {
+        todo!()
+    }
 }
 
 // CodaComponentName
 
-pub struct CodaComponentName {}
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct CodaComponentName {
+    pub string: String,
+}
+
+// OcamlExpr
+
+impl OcamlExpr {
+    pub fn coda_var(e: OcamlExpr) -> OcamlExpr {
+        OcamlExpr::app(OcamlName::new("var"), vec![e])
+    }
+
+    pub fn coda_star() -> OcamlExpr {
+        OcamlExpr::var(OcamlName::new("star"))
+    }
+
+    pub fn coda_tuple(es: Vec<OcamlExpr>) -> OcamlExpr {
+        OcamlExpr::app(OcamlName::new("Expr.tuple"), es)
+    }
+
+    pub fn coda_let(x: OcamlExpr, e1: OcamlExpr, e2: OcamlExpr) -> OcamlExpr {
+        OcamlExpr::app(OcamlName::new("elet"), vec![x, e1, e2])
+    }
+}
--- code_producers/src/coda_elements/ocaml.rs
+++ code_producers/src/coda_elements/ocaml.rs
@@ -1,1 +1,179 @@
+// OcamlMod
 
+pub struct OcamlMod {
+    pub stmts: Vec<OcamlStmt>,
+}
+
+impl OcamlMod {
+    pub fn ocaml_compile(&self) -> String {
+        let mut result = String::new();
+        for stmt in &self.stmts {
+            result.push_str(&stmt.ocaml_compile());
+            result.push_str("\n\n")
+        }
+        result
+    }
+}
+
+// OcamlStmt
+
+pub enum OcamlStmt {
+    Open(OcamlName),
+    Define(OcamlName, OcamlExpr),
+}
+
+impl OcamlStmt {
+    pub fn ocaml_compile(&self) -> String {
+        match &self {
+            OcamlStmt::Open(m) => format!("open {}", m.ocaml_compile()),
+            OcamlStmt::Define(x, e) => {
+                format!("let {} = {}", x.ocaml_compile(), e.ocaml_compile())
+            }
+        }
+    }
+}
+
+// OcamlExpr
+
+pub enum OcamlExpr {
+    Def(OcamlName, Box<OcamlExpr>, Box<OcamlExpr>),
+    App(OcamlName, Vec<Box<OcamlExpr>>),
+    Op(OcamlOp, Vec<Box<OcamlExpr>>),
+    String(String),
+    Number(String),
+}
+
+impl OcamlExpr {
+    pub fn string(str: &str) -> OcamlExpr {
+        OcamlExpr::String(str.to_string())
+    }
+
+    pub fn number(str: &str) -> OcamlExpr {
+        OcamlExpr::Number(str.to_string())
+    }
+
+    pub fn var(name: OcamlName) -> OcamlExpr {
+        OcamlExpr::App(name, vec![])
+    }
+
+    pub fn app(name: OcamlName, args: Vec<OcamlExpr>) -> OcamlExpr {
+        let mut args_new = Vec::new();
+        for arg in args {
+            args_new.push(Box::new(arg))
+        }
+        OcamlExpr::App(name, args_new)
+    }
+
+    pub fn def(name: OcamlName, imp: OcamlExpr, body: OcamlExpr) -> OcamlExpr {
+        OcamlExpr::Def(name, Box::new(imp), Box::new(body))
+    }
+
+    pub fn op(op: OcamlOp, args: Vec<OcamlExpr>) -> OcamlExpr {
+        let mut args_new = Vec::new();
+        for arg in args {
+            args_new.push(Box::new(arg))
+        }
+        OcamlExpr::Op(op, args_new)
+    }
+
+    pub fn ocaml_compile(&self) -> String {
+        match &self {
+            OcamlExpr::Def(x, e1, e2) => format!(
+                "let {} = {} in @@\n  {}",
+                x.ocaml_compile(),
+                e1.ocaml_compile(),
+                e2.ocaml_compile()
+            ),
+            OcamlExpr::App(x, es) => {
+                if es.len() == 0 {
+                    format!("{}", x.ocaml_compile())
+                } else {
+                    format!(
+                        "({} {})",
+                        x.ocaml_compile(),
+                        es.iter().map(|e| e.ocaml_compile()).collect::<Vec<String>>().join(" ")
+                    )
+                }
+            }
+            OcamlExpr::Op(o, es) => o.ocaml_compile(es.iter().map(|e| e.ocaml_compile()).collect()),
+            OcamlExpr::String(str) => format!("\"{}\"", str),
+            OcamlExpr::Number(str) => str.to_string(),
+        }
+    }
+}
+
+// OcamlName
+
+pub struct OcamlName {
+    pub string: String,
+}
+
+impl OcamlName {
+    pub fn new(str: &str) -> Self {
+        let str = str.replace("[", "_").replace("]", "_");
+        for (i, c) in str.chars().enumerate() {
+            if !c.is_ascii() {
+                panic!("Invalid OCaml name (cannot have non-ASCII character): {}", str)
+            }
+            if i == 0 {
+                if c.is_numeric() {
+                    panic!("Invalid OCaml name (cannot start with numeric character): {}", str)
+                }
+            }
+        }
+        Self { string: str.to_string() }
+    }
+
+    pub fn ocaml_compile(&self) -> String {
+        format!("{}", self.string)
+    }
+}
+
+// OcamlOp
+
+pub struct OcamlOp {
+    pub qualifier: Option<OcamlName>,
+    pub string: String,
+}
+
+const OCAML_OP_VALID_CHARS: [char; 15] =
+    ['!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '=', '<', '>', '?', '/'];
+
+impl OcamlOp {
+    pub fn new(qualifier: Option<OcamlName>, str: &str) -> Self {
+        for c in str.chars() {
+            if !OCAML_OP_VALID_CHARS.contains(&c) {
+                panic!("Invalid OCaml operator: {}", str)
+            }
+        }
+        Self { qualifier, string: str.to_string() }
+    }
+
+    pub fn ocaml_compile(&self, strs: Vec<String>) -> String {
+        if strs.len() == 0 {
+            panic!("An infixed operation must have at least one argument")
+        } else if strs.len() == 1 {
+            let str0 = &strs[0];
+            let str1 = &strs[1];
+            match &self.qualifier {
+                Some(q) => {
+                    format!("{}.({} {} {})", q.ocaml_compile(), str0, self.string, str1)
+                }
+                None => {
+                    format!("({} {} {})", str0, self.string, str1)
+                }
+            }
+        } else {
+            let str = strs.join(&format!(" {} ", self.string));
+
+            match &self.qualifier {
+                Some(q) => {
+                    format!("{}.({})", q.ocaml_compile(), str)
+                }
+                None => {
+                    format!("({})", str)
+                }
+            }
+        }
+    }
+}
