6acecb5f63967a275d93fcdf35767df761205f5a
Tim Hoffman <timothy.hoffman@veridise.com>
2024-07-15 15:55:25 -0500

    Remove unnecessary implementation from FunctionEnvData

--- circuit_passes/src/bucket_interpreter/env/function_env.rs
+++ circuit_passes/src/bucket_interpreter/env/function_env.rs
@@ -1,27 +1,21 @@
 use std::cell::Ref;
-use std::collections::{BTreeMap, HashMap, hash_map::Entry};
+use std::collections::{BTreeMap, HashMap};
 use std::fmt::{Display, Formatter};
 use compiler::circuit_design::function::FunctionCode;
 use compiler::circuit_design::template::TemplateCode;
 use compiler::intermediate_representation::ir_interface::AddressType;
 use compiler::intermediate_representation::BucketId;
-use crate::bucket_interpreter::error::{new_inconsistency_err, BadInterp};
+use crate::bucket_interpreter::error::BadInterp;
 use crate::bucket_interpreter::write_collector::Writes;
 use crate::bucket_interpreter::{BucketInterpreter, CALL_STACK_LIMIT};
 use crate::bucket_interpreter::value::Value;
-use super::{
-    sort, CallStack, CallStackFrame, Env, EnvContextKind, LibraryAccess, SubcmpEnv,
-    PRINT_ENV_SORTED,
-};
+use super::{sort, CallStack, CallStackFrame, Env, EnvContextKind, LibraryAccess, PRINT_ENV_SORTED};
 
 /// This Env is used for Circom source functions.
 #[derive(Clone)]
 pub struct FunctionEnvData<'a> {
     caller_stack: Vec<BucketId>,
     vars: HashMap<usize, Value>,
-    signals: HashMap<usize, Value>,
-    subcmps: HashMap<usize, SubcmpEnv>,
-    subcmp_names: HashMap<usize, String>,
     libs: &'a dyn LibraryAccess,
     /// This call stack is used to prevent the Rust stack from overflowing due to recursive
     /// calls in the Circom source where termination cannot be determined by the intepreter.
@@ -37,18 +31,15 @@ impl Display for FunctionEnvData<'_> {
         if PRINT_ENV_SORTED {
             write!(
                 f,
-                "FunctionEnv{{\n  ctx = {:?}\n  vars = {:?}\n  signals = {:?}\n  names = {:?}\n  subcmps = {:?}\n}}",
+                "FunctionEnv{{\n  ctx = {:?}\n  vars = {:?}\n}}",
                 self.caller_stack,
                 sort(&self.vars, std::convert::identity),
-                sort(&self.signals, std::convert::identity),
-                sort(&self.subcmp_names, std::convert::identity),
-                sort(&self.subcmps, std::convert::identity)
             )
         } else {
             write!(
                 f,
-                "FunctionEnv{{\n  ctx = {:?}\n  vars = {:?}\n  signals = {:?}\n  names = {:?}\n  subcmps = {:?}\n}}",
-                self.caller_stack, self.vars, self.signals, self.subcmp_names, self.subcmps
+                "FunctionEnv{{\n  ctx = {:?}\n  vars = {:?}\n}}",
+                self.caller_stack, self.vars,
             )
         }
     }
@@ -73,15 +64,7 @@ impl<'a> FunctionEnvData<'a> {
     ) -> Self {
         let mut caller_stack = base.get_caller_stack().to_vec();
         caller_stack.push(*caller);
-        FunctionEnvData {
-            caller_stack,
-            call_stack,
-            vars: Default::default(),
-            signals: Default::default(),
-            subcmps: Default::default(),
-            subcmp_names: Default::default(),
-            libs,
-        }
+        FunctionEnvData { caller_stack, call_stack, vars: Default::default(), libs }
     }
 
     // READ OPERATIONS
@@ -108,32 +91,39 @@ impl<'a> FunctionEnvData<'a> {
         self.vars.get(&idx).unwrap_or_default().clone()
     }
 
-    pub fn get_signal(&self, idx: usize) -> Value {
-        self.signals.get(&idx).unwrap_or_default().clone()
+    pub fn get_signal(&self, _idx: usize) -> Value {
+        // There are no signals in source functions.
+        unreachable!()
     }
 
-    pub fn get_subcmp_signal(&self, subcmp_idx: usize, signal_idx: usize) -> Value {
-        self.subcmps[&subcmp_idx].get_signal(signal_idx)
+    pub fn get_subcmp_signal(&self, _subcmp_idx: usize, _signal_idx: usize) -> Value {
+        // There are no components in source functions.
+        unreachable!()
     }
 
-    pub fn get_subcmp_name(&self, subcmp_idx: usize) -> &String {
-        &self.subcmp_names[&self.subcmps[&subcmp_idx].template_id]
+    pub fn get_subcmp_name(&self, _subcmp_idx: usize) -> &String {
+        // There are no components in source functions.
+        unreachable!()
     }
 
-    pub fn get_subcmp_template_id(&self, subcmp_idx: usize) -> usize {
-        self.subcmps[&subcmp_idx].template_id
+    pub fn get_subcmp_template_id(&self, _subcmp_idx: usize) -> usize {
+        // There are no components in source functions.
+        unreachable!()
     }
 
-    pub fn get_subcmp_counter(&self, subcmp_idx: usize) -> Value {
-        Value::KnownU32(self.subcmps.get(&subcmp_idx).unwrap().get_counter())
+    pub fn get_subcmp_counter(&self, _subcmp_idx: usize) -> Value {
+        // There are no components in source functions.
+        unreachable!()
     }
 
-    pub fn subcmp_counter_is_zero(&self, subcmp_idx: usize) -> bool {
-        self.subcmps.get(&subcmp_idx).unwrap().counter_is_zero()
+    pub fn subcmp_counter_is_zero(&self, _subcmp_idx: usize) -> bool {
+        // There are no components in source functions.
+        unreachable!()
     }
 
-    pub fn subcmp_counter_equal_to(&self, subcmp_idx: usize, value: usize) -> bool {
-        self.subcmps.get(&subcmp_idx).unwrap().counter_equal_to(value)
+    pub fn subcmp_counter_equal_to(&self, _subcmp_idx: usize, _value: usize) -> bool {
+        // There are no components in source functions.
+        unreachable!()
     }
 
     pub fn get_vars_sort(&self) -> BTreeMap<usize, Value> {
@@ -160,10 +150,9 @@ impl<'a> FunctionEnvData<'a> {
         copy
     }
 
-    pub fn set_signal(self, idx: usize, value: Value) -> Self {
-        let mut copy = self;
-        copy.signals.insert(idx, value);
-        copy
+    pub fn set_signal(self, _idx: usize, _value: Value) -> Self {
+        // There are no signals in source functions.
+        unreachable!()
     }
 
     pub fn set_vars_to_unk<T: IntoIterator<Item = usize>>(self, idxs: Option<T>) -> Self {
@@ -181,69 +170,46 @@ impl<'a> FunctionEnvData<'a> {
     }
 
     pub fn set_signals_to_unknown<T: IntoIterator<Item = usize>>(self, idxs: Option<T>) -> Self {
-        let mut copy = self;
-        if let Some(idxs) = idxs {
-            for idx in idxs {
-                copy.signals.insert(idx, Value::Unknown);
-            }
-        } else {
-            for (_, v) in copy.signals.iter_mut() {
-                *v = Value::Unknown;
-            }
-        }
-        copy
+        // There are no signals in source functions.
+        // This function is not unreachable because it may be called without actually
+        //  encountering any signal references, thus the input must be None or empty.
+        assert!(idxs.is_none() || idxs.is_some_and(|e| e.into_iter().next().is_none()));
+        self
     }
 
     pub fn set_subcmps_to_unk<T: IntoIterator<Item = usize>>(
         self,
-        subcmp_idxs: Option<T>,
+        idxs: Option<T>,
     ) -> Result<Self, BadInterp> {
-        let mut copy = self;
-        if let Some(idxs) = subcmp_idxs {
-            for idx in idxs {
-                copy = copy.update_subcmp(idx, SubcmpEnv::reset)?;
-            }
-        } else {
-            for (_, v) in copy.subcmps.iter_mut() {
-                v.signals.clear();
-            }
-        }
-        Ok(copy)
+        // There are no components in source functions.
+        // This function is not unreachable because it may be called without actually
+        //  encountering any signal references, thus the input must be None or empty.
+        assert!(idxs.is_none() || idxs.is_some_and(|e| e.into_iter().next().is_none()));
+        Ok(self)
     }
 
     pub fn set_subcmp_signal(
         self,
-        subcmp_idx: usize,
-        signal_idx: usize,
-        value: Value,
+        _subcmp_idx: usize,
+        _signal_idx: usize,
+        _value: Value,
     ) -> Result<Self, BadInterp> {
-        self.update_subcmp(subcmp_idx, |e| e.set_signal(signal_idx, value))
+        // There are no components in source functions.
+        unreachable!()
     }
 
-    pub fn set_subcmp_counter(self, subcmp_idx: usize, new_val: usize) -> Result<Self, BadInterp> {
-        self.update_subcmp(subcmp_idx, |e| e.set_counter(new_val))
-    }
-
-    pub fn decrease_subcmp_counter(self, subcmp_idx: usize) -> Result<Self, BadInterp> {
-        self.update_subcmp(subcmp_idx, SubcmpEnv::decrease_counter)
-    }
-
-    fn update_subcmp(
+    pub fn set_subcmp_counter(
         self,
-        subcmp_idx: usize,
-        f: impl FnOnce(&mut SubcmpEnv),
+        _subcmp_idx: usize,
+        _new_val: usize,
     ) -> Result<Self, BadInterp> {
-        let mut copy = self;
-        match copy.subcmps.entry(subcmp_idx) {
-            Entry::Occupied(mut entry) => {
-                f(entry.get_mut());
-                Ok(copy)
-            }
-            Entry::Vacant(_) => Result::Err(new_inconsistency_err(format!(
-                "Can't find subcomponent {}",
-                subcmp_idx
-            ))),
-        }
+        // There are no components in source functions.
+        unreachable!()
+    }
+
+    pub fn decrease_subcmp_counter(self, _subcmp_idx: usize) -> Result<Self, BadInterp> {
+        // There are no components in source functions.
+        unreachable!()
     }
 
     pub fn run_subcmp(
@@ -252,35 +218,18 @@ impl<'a> FunctionEnvData<'a> {
         _name: &String,
         _interpreter: &BucketInterpreter,
     ) -> Self {
-        // The env returns Unknown by default to any index that does not have a value
-        // So we can fake executing a subcomponent and any read to the output
-        // of a subcomponent will return Unknown which is the only value that signals can have.
-        self
+        // There are no components in source functions.
+        unreachable!()
     }
 
     pub fn create_subcmp(
         self,
-        name: &String,
-        base_index: usize,
-        count: usize,
-        template_id: usize,
+        _name: &String,
+        _base_index: usize,
+        _count: usize,
+        _template_id: usize,
     ) -> Self {
-        let mut copy = self;
-
-        match copy.subcmp_names.get(&template_id) {
-            None => {
-                copy.subcmp_names.insert(template_id, name.clone());
-            }
-            Some(old) => {
-                assert_eq!(old, name);
-            }
-        }
-
-        let number_of_inputs = copy.get_template(name).number_of_inputs;
-        for i in base_index..(base_index + count) {
-            let old = copy.subcmps.insert(i, SubcmpEnv::new(number_of_inputs, template_id));
-            assert!(old.is_none()); //no keys are overwritten
-        }
-        copy
+        // There are no components in source functions.
+        unreachable!()
     }
 }
